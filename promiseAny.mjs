f6ca37a44d44b6910096e2526f5e81d771c446d6
Nazar Vovk <nvovk.2004@gmail.com>
2022-06-08 14:52:35 +0300

    format code

--- promiseAny.mjs
+++ promiseAny.mjs
@@ -5,18 +5,18 @@
 
 const p1 = new Promise((resolve) => {
   setTimeout(() => {
-    resolve('Promise 1 resolved'), Math.random() * 1000)
-  };
+    resolve('Promise 1 resolved');
+  }, Math.random() * 1000);
 });
 const p2 = new Promise((resolve) => {
   setTimeout(() => {
-    resolve('Promise 2 resolved'), Math.random() * 1000)
-  };
+    resolve('Promise 2 resolved');
+  }, Math.random() * 1000);
 });
 const p3 = new Promise((resolve) => {
   setTimeout(() => {
-    resolve('Promise 3 resolved'), Math.random() * 1000)
-  };
+    resolve('Promise 3 resolved');
+  }, Math.random() * 1000);
 });
 
 const res = await Promise.any([p1, p2, p3]);
